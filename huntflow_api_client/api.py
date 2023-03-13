37c5e996107af77d01b7bc57ba4674a62369aa27
Aleksey Reznikov <reznikov@huntflow.ru>
2023-03-13 08:38:44 +0300

    entities

--- huntflow_api_client/api.py
+++ huntflow_api_client/api.py
@@ -13,15 +13,13 @@ class HuntflowAPI:
     ):
         self.base_url = base_url
         self.token = token
-        self.http_client = self.init_http_client(request_event_hooks, response_event_hooks)
+        self._request_event_hooks = request_event_hooks or []
+        self._response_event_hooks = response_event_hooks or []
 
-    def init_http_client(
-        self,
-        request_event_hooks: t.List[t.Callable] = None,
-        response_event_hooks: t.List[t.Callable] = None,
-    ) -> httpx.AsyncClient:
+    @property
+    def client(self) -> httpx.AsyncClient:
         headers = {"Authorization": f"Bearer {self.token}"}
         http_client = httpx.AsyncClient(base_url=self.base_url, headers=headers)
-        http_client.event_hooks["request"] = request_event_hooks or []
-        http_client.event_hooks["response"] = response_event_hooks or []
+        http_client.event_hooks["request"] = self._request_event_hooks
+        http_client.event_hooks["response"] = self._response_event_hooks
         return http_client
