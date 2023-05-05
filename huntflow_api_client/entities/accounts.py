dd58ecd215e9d5485acf11d5ab5ce8ff5215a23f
koval <koval@huntflow.ru>
2023-05-05 15:00:41 +0300

    Add docstrings.

--- huntflow_api_client/entities/accounts.py
+++ huntflow_api_client/entities/accounts.py
@@ -8,14 +8,20 @@ from huntflow_api_client.models.response.accounts import (
 
 class Account(BaseEntity):
     async def get_current_user(self) -> MeResponse:
-        path = "/me"
+        """
+        :return: Information about the current user
+        """
         response = await self._api.request(
             "GET",
-            path,
+            "/me",
         )
         return MeResponse.parse_obj(response.json())
 
     async def available_org_list(self) -> OrganizationsListResponse:
+        """
+        :return: List of available organizations for the
+        user associated with the passed authentication
+        """
         path = "/accounts"
         response = await self._api.request(
             "GET",
@@ -24,6 +30,10 @@ class Account(BaseEntity):
         return OrganizationsListResponse.parse_obj(response.json())
 
     async def get_org_info(self, account_id: int) -> OrganizationInfoResponse:
+        """
+       :param account_id: Organization ID
+       :return: Information about the specified organization
+       """
         path = f"/accounts/{account_id}"
         response = await self._api.request(
             "GET",
