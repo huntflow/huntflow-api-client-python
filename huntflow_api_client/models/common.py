26303fd66541d1e781d275f39c87d9d063defc3b
koval <koval@huntflow.ru>
2023-05-10 16:33:56 +0300

    Add Optional in the Applicant model fields.

--- huntflow_api_client/models/common.py
+++ huntflow_api_client/models/common.py
@@ -44,11 +44,15 @@ class PaginatedResponse(BaseModel):
 class Applicant(BaseModel):
     first_name: Optional[str] = Field(None, description="First name", example="John")
     last_name: Optional[str] = Field(None, description="Last name", example="Doe")
-    middle_name: str = Field(None, description="Middle name", example="Michael")
-    money: str = Field(None, description="Salary expectation", example="$100000")
-    phone: str = Field(None, description="Phone number", example="89999999999")
-    email: EmailStr = Field(None, description="Email address", example="mail@mail.ru")
-    skype: str = Field(None, description="Skype login", example="my_skype")
-    position: str = Field(None, description="Applicant’s occupation", example="Front-end developer")
-    company: str = Field(None, description="Applicant’s place of work", example="Google Inc.")
-    photo: int = Field(None, description="Applicant’s photo ID", example=1)
+    middle_name: Optional[str] = Field(None, description="Middle name", example="Michael")
+    money: Optional[str] = Field(None, description="Salary expectation", example="$100000")
+    phone: Optional[str] = Field(None, description="Phone number", example="89999999999")
+    email: Optional[EmailStr] = Field(None, description="Email address", example="mail@mail.ru")
+    skype: Optional[str] = Field(None, description="Skype login", example="my_skype")
+    position: Optional[str] = Field(
+        None, description="Applicant’s occupation", example="Front-end developer",
+    )
+    company: Optional[str] = Field(
+        None, description="Applicant’s place of work", example="Google Inc.",
+    )
+    photo: Optional[int] = Field(None, description="Applicant’s photo ID", example=1)
