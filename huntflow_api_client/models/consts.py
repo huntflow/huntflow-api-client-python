134207b8c034b83f179facedd3209e82120d0b17
koval <koval@huntflow.ru>
2023-05-10 15:42:24 +0300

    Add AgreementState enum.

--- huntflow_api_client/models/consts.py
+++ huntflow_api_client/models/consts.py
@@ -13,3 +13,10 @@ class MemberType(str, Enum):
     owner = "owner"
     manager = "manager"
     watcher = "watcher"
+
+
+class AgreementState(str, Enum):
+    not_sent = "not_sent"
+    sent = "sent"
+    accepted = "accepted"
+    declined = "declined"
