961593a72f51278af3118669a3e7879b4c477a80
KlochkovHF <klochkov@huntflow.ru>
2023-05-18 12:38:58 +0300

    Add delayed task entity

---
+++ huntflow_api_client/entities/__init__.py
@@ -0,0 +1,31 @@
+from huntflow_api_client.entities.account_vacancy_request import AccountVacancyRequest
+from huntflow_api_client.entities.accounts import Account
+from huntflow_api_client.entities.applicant_on_vacancy import ApplicantOnVacancy
+from huntflow_api_client.entities.applicant_on_vacancy_status import ApplicantOnVacancyStatus
+from huntflow_api_client.entities.coworkers import Coworker
+from huntflow_api_client.entities.delayed_tasks import DelayedTask
+from huntflow_api_client.entities.dictionaries import Dictionary
+from huntflow_api_client.entities.divisions import AccountDivision
+from huntflow_api_client.entities.production_calendars import ProductionCalendar
+from huntflow_api_client.entities.rejection_reason import RejectionReason
+from huntflow_api_client.entities.tags import AccountTag
+from huntflow_api_client.entities.vacancies import Vacancy
+from huntflow_api_client.entities.vacancy_requests import VacancyRequest
+from huntflow_api_client.entities.webhooks import Webhook
+
+__all__ = (
+    "AccountVacancyRequest",
+    "Account",
+    "ApplicantOnVacancy",
+    "ApplicantOnVacancyStatus",
+    "Coworker",
+    "Dictionary",
+    "AccountDivision",
+    "ProductionCalendar",
+    "RejectionReason",
+    "AccountTag",
+    "VacancyRequest",
+    "Webhook",
+    "DelayedTask",
+    "Vacancy",
+)
---
+++ tests/test_entities/test_delayed_tasks.py
@@ -0,0 +1,42 @@
+from pytest_httpx import HTTPXMock
+
+from huntflow_api_client import HuntflowAPI
+from huntflow_api_client.entities import DelayedTask
+from huntflow_api_client.models.response.delayed_tasks import DelayedTaskResponse
+from huntflow_api_client.tokens.proxy import HuntflowTokenProxy
+from tests.api import BASE_URL
+
+DELAYED_TASKS_RESPONSE = {
+    "task_id": "1882e199-a310-4000-8706-490f0822fa01",
+    "state": "enqueued",
+    "created": 1561006501.318867,
+    "updated": 1561006501.318867,
+    "created_datetime": "2020-01-01T00:00:00+03:00",
+    "updated_datetime": "2020-01-01T00:00:00+03:00",
+    "states_log": [
+        {
+            "state": "enqueued",
+            "timestamp": 1561006501.318867,
+            "datetime": "2020-01-01T00:00:00+03:00",
+            "comment": "Some text",
+        },
+    ],
+}
+
+
+ACCOUNT_ID = 1
+TASK_ID = "1"
+
+
+async def test_get_delayed_task(
+    httpx_mock: HTTPXMock,
+    token_proxy: HuntflowTokenProxy,
+) -> None:
+    httpx_mock.add_response(
+        url=f"{BASE_URL}/accounts/{ACCOUNT_ID}/delayed_tasks/{TASK_ID}",
+        json=DELAYED_TASKS_RESPONSE,
+    )
+    api_client = HuntflowAPI(BASE_URL, token_proxy=token_proxy)
+    tasks = DelayedTask(api_client)
+    response = await tasks.get(ACCOUNT_ID, TASK_ID)
+    assert response == DelayedTaskResponse.parse_obj(DELAYED_TASKS_RESPONSE)
