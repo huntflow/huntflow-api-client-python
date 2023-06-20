20bd9623be5f7871d3c565a3b33de38232aff4e1
koval <koval@huntflow.ru>
2023-06-20 09:42:52 +0300

    Use MemberType in UserResponse model.

--- huntflow_api_client/models/response/users.py
+++ huntflow_api_client/models/response/users.py
@@ -2,13 +2,14 @@ from typing import List, Optional
 
 from pydantic import BaseModel, EmailStr, Field
 
+from huntflow_api_client.models.consts import MemberType
 from huntflow_api_client.models.response.coworkers import Permission
 
 
 class UserResponse(BaseModel):
     id: int = Field(..., description="User ID")
     name: Optional[str] = Field(None, description="User name")
-    member_type: str = Field(..., alias="type", description="User type (role)")
+    type: MemberType = Field(..., description="User type (role)")
     head: Optional[int] = Field(None, description="Head user ID")
     email: Optional[EmailStr] = Field(None, description="Email")
     meta: Optional[dict] = Field(None, description="Additional meta information")
