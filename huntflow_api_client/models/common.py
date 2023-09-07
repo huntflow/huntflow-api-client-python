3ac320f5a7aa1c13da9fa99bcac1cb1180aac25d
koval <koval@huntflow.ru>
2023-09-07 14:37:18 +0300

    Fix IncEx typehint.

--- huntflow_api_client/models/common.py
+++ huntflow_api_client/models/common.py
@@ -1,6 +1,6 @@
 import json
 from datetime import date, datetime
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Set, Union
 
 import typing_extensions
 from pydantic import AnyHttpUrl, BaseModel, EmailStr, Field, PositiveInt
@@ -12,7 +12,7 @@ from huntflow_api_client.models.consts import (
     VacancyState,
 )
 
-IncEx: typing_extensions.TypeAlias = "set[int] | set[str] | dict[int, Any] | dict[str, Any] | None"
+IncEx: typing_extensions.TypeAlias = "Set[int] | Set[str] | Dict[int, Any] | Dict[str, Any] | None"
 
 
 class JsonRequestModel(BaseModel):
