ad8533396684416deda6646b8d162eb93205f05f
Aleksandr Kroshko <kroshko@huntflow.ru>
2026-04-06 11:05:55 +0300

    DEV-22328: mypy fix

--- huntflow_api_client/utils.py
+++ huntflow_api_client/utils.py
@@ -1,9 +1,12 @@
 from enum import Enum
+from typing import Any, Callable, Dict, Type, TypeVar
+
+T = TypeVar("T", bound=Enum)
 
 
-def extend_enum(inherited_enum):
-    def wrapper(added_enum):
-        joined = {}
+def extend_enum(inherited_enum: Type[T]) -> Callable:
+    def wrapper(added_enum: Type[T]) -> Enum:
+        joined: Dict[str, Any] = {}
         for item in inherited_enum:
             joined[item.name] = item.value
         for item in added_enum:
